2a14a4dd6e540b715caed8dd6d3698c329b3e9a7
Iuri Meresan <iuri.meresan@esolutions.ro>
2023-10-16 10:03:28 +0300

    Temporary simple autopilot cluster

--- terraform/gcp/main.tf
+++ terraform/gcp/main.tf
@@ -1,17 +1,108 @@
-# module "kubernetes-engine" {
+# locals {
+#   zones = ["${var.region}-a"]
+# }
+
+# module "gke" {
 #   source  = "terraform-google-modules/kubernetes-engine/google"
 #   version = "27.0.0"
 
-#   project_id = var.project
+#   project_id = var.project_id
 
-#   name = var.name
+#   name   = var.name
+#   region = var.region
+#   zones  = local.zones
 
-#   network = var.network
+#   network    = var.network
 #   subnetwork = var.subnetwork
 
-#   ip_range_pods = var.ip_range_pods
-#   ip_range_services = var.ip_range_services
+#   ip_range_pods     = ""
+#   ip_range_services = ""
+
+#   # master_authorized_networks = [
+#   #   {
+#   #     cidr_block   = data.google_compute_subnetwork.subnetwork.ip_cidr_range
+#   #     display_name = "VPC"
+#   #   },
+#   # ]
+
+#   horizontal_pod_autoscaling = true
+
+#   node_pools = [
+#     {
+#       name               = "default-node-pool"
+#       machine_type       = var.machine_type
+#       node_locations     = join(",", local.zones)
+#       min_count          = 1
+#       max_count          = 3
+#       # local_ssd_count    = 10
+#       # disk_type          = "pd-standard"
+#       # image_type         = "COS_CONTAINERD"
+#       auto_repair        = true
+#       auto_upgrade       = true
+#       service_account    = var.service_account
+#       preemptible        = false
+#       initial_node_count = 1
+#     },
+#   ]
+
+#   node_pools_oauth_scopes = {
+#     all = []
+
+#     default-node-pool = [
+#       "https://www.googleapis.com/auth/cloud-platform",
+#     ]
+#   }
+
+#   node_pools_labels = {
+#     all = {}
+
+#     default-node-pool = {
+#       default-node-pool = true
+#     }
+#   }
+
+#   node_pools_metadata = {
+#     all = {}
+
+#     default-node-pool = {
+#       node-pool-metadata-custom-value = "my-node-pool"
+#     }
+#   }
+
+#   node_pools_taints = {
+#     all = []
+
+#     default-node-pool = [
+#       {
+#         key    = "default-node-pool"
+#         value  = true
+#         effect = "PREFER_NO_SCHEDULE"
+#       },
+#     ]
+#   }
+
+#   node_pools_tags = {
+#     all = []
+
+#     default-node-pool = [
+#       "default-node-pool",
+#     ]
+#   }
 # }
-resource "google_compute_network" "vpc_network" {
-  name = "terraform-network"
+
+
+# # module "vpc" {
+# #   source  = "terraform-google-modules/network/google"
+# #   version = "~> 7.3"
+
+# #   project_id   = var.project_id
+# #   network_name = "vpc"
+# # }
+
+
+
+module "gke" {
+  source = "terraform-google-modules/kubernetes-engine/google//examples/simple_autopilot_public"
+  project_id = var.project_id
+  region = var.region
 }
--- terraform/gcp/versions.tf
+++ terraform/gcp/versions.tf
@@ -4,11 +4,20 @@ terraform {
       source  = "hashicorp/google"
       version = "4.77.0"
     }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+    }
   }
 }
 
 provider "google" {
-  project = var.project
+  project = var.project_id
   region  = var.region
-  zone    = var.zone
+  # zone    = var.zone
+}
+
+provider "kubernetes" {
+  host                   = "https://${module.gke.endpoint}"
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(module.gke.ca_certificate)
 }
